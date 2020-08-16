5620aa5c7ef209f19ae3d7aac747a418429b43b4
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-08-16 21:25:26 +0100

    README.md

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""function that returns a solution to minimum operation of copy all
+    and paste of character H given in the problem
+"""
---
+++ 0x04-sandpiles/README.md
@@ -0,0 +1,5 @@
+# 0x04-sandpiles
+
+## Introduction :
+
+watch this [video](https://www.youtube.com/watch?v=1MtEUErz7Gg)

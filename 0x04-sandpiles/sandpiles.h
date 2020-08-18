4efad1bbec7898a7fd5ae3d50fd4469f3f36bf5e
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-08-18 23:39:03 +0100

    sandpiles.h

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -4,6 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 static void print_grid(int grid[3][3]);
 #endif /* SANDPILES_H */

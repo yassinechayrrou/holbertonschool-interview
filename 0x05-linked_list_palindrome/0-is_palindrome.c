6a7686ed61e8792cf227128737e3b19a28181aac
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-08-24 22:35:36 +0100

    check palindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -6,21 +6,40 @@
  */
 int is_palindrome(listint_t **head)
 {
-	return (checkPalindrome(*head));
+	listint_t *slow = *head, *fast = *head;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	slow = reversePalindrome(slow);
+	fast = *head;
+
+	while (slow != NULL)
+	{
+		if (slow->n != fast->n)
+			return (0);
+		slow = slow->next;
+		fast = fast->next;
+	}
+	return (1);
 }
 /**
- *checkPalindrome - stacks LL recursively (reverse) and compares with LL
+ *reversePalindrome - stacks LL recursively (reverse) and compares with LL
  *@head: pointer to head of linked list
- *Return: 1 or 0
+ *Return: pointer to listint_t 
  */
-int checkPalindrome(listint_t *head)
+listint_t *reversePalindrome(listint_t *head)
 {
-	listint_t *temp;
+	listint_t *previous = NULL, *next = NULL, *current = head;
 
-	temp = head;
-	if (temp == NULL)
-		return (1);
-	checkPalindrome(temp->next) && temp->n == head->n;
-
-
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = previous;
+		previous = current;
+		current = next;
+	}
+	return (previous);
 }
--- 0x05-linked_list_palindrome/main.c
+++ 0x05-linked_list_palindrome/main.c
@@ -22,7 +22,6 @@ int main(void)
 	add_nodeint_end(&head, 972);
 	add_nodeint_end(&head, 17);
 	add_nodeint_end(&head, 1);
-
 	print_listint(head);
 
 	if (is_palindrome(&head) == 1)

538a34042d71d6f6b07bf866817630d6765f3eb5
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-10-08 19:20:47 +0100

    inefficient code: require recursion method for a better optimization but still works

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -9,13 +9,25 @@ int is_palindrome(unsigned long n)
 	int i, len;
 	char buffer[sizeof(unsigned long) * 8 + 1];
 
-	sprintf(buffer, "%lu", n);
-	for (len = 0; buffer[len]; ++len)
-		len += 1;
+	len = strlen(yassineItoa(n, buffer));
 	for (i = 0; i < len / 2; ++i)
 	{
 		if (buffer[i] != buffer[len - 1 - i])
 			return (0);
 	}
 	return (1);
+}
+
+/**
+ * yassineItoa - converts unsigned long to string
+ * @n: take unsigned long
+ * @string: takes pointer to char
+ * Return: pointer to char (string)
+ */
+char *yassineItoa(unsigned long n, char *string)
+{
+	if (string == NULL)
+		return (NULL);
+	sprintf(string, "%lu", n);
+	return (string);
 }
--- 0x08-palindrome_integer/palindrome.h
+++ 0x08-palindrome_integer/palindrome.h
@@ -3,7 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int is_palindrome(unsigned long n);
+char *yassineItoa(unsigned long n, char *string);
 
 #endif /* palindrome headerFile */

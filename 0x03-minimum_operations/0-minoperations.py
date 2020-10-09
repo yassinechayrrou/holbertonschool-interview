3f9b7e95638869731101b1d76ca98517cb451adf
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-10-09 20:39:41 +0100

    quadratic time solution

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -2,3 +2,14 @@
 """function that returns a solution to minimum operation of copy all
     and paste of character H given in the problem
 """
+
+
+def minOperations(n):
+    if n < 2:
+        return 0
+    result = 0
+    for i in range(2, n + 1):
+        while n % i == 0:
+            result += i
+            n = n // i
+    return result

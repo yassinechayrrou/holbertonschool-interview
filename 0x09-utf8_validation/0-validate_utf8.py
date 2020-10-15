4db47fa4cb60bd98bae7a7bf3fa4b4704a3b9b83
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-10-16 00:53:44 +0100

    check utfvalidation

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+script that presents a method that determines if a given data set represents
+a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    bytesNumber = 0
+    for i in range(len(data)):
+        number = data[i]
+        if number >= 255:
+            return 'false'
+        elif number & 128 == 0:
+            bytesNumber = 1
+        elif number & 224 == 192:
+            bytesNumber = 2
+        elif number & 240 == 224:
+            bytesNumber = 3
+        elif number & 248 == 240:
+            bytesNumber = 4
+        else:
+            return 'false'
+        for c in range(1, bytesNumber):
+            if i + c >= len(data):
+                return 'false'
+            elif data[i + c] & 197 != 128:
+                return 'false'
+        i = i + bytesNumber - 1
+    return 'true'

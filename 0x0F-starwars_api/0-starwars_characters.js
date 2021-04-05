a9e3b6e78a164cef4b7cbef50c331e98403745af
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-04-05 23:12:41 +0100

    used promise to resolve requests

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -6,34 +6,26 @@ const URL = 'https://swapi-api.hbtn.io/api/films/';
 const movieID = process.argv[2];
 const Req = URL + movieID;
 
-function callback (err, res, body) {
+if (!movieID) {
+    process.exit()
+}
+async function callback (err, res, body) {
   if (err) {
-    console.log(err);
-    process.exit();
+    return console.log(err);
   }
-  const movieName = JSON.parse(body).title;
-  request(URL, (err, res, body) => {
-    if (err) {
-      console.log(err);
-      process.exit();
-    }
-    const movieList = JSON.parse(body).results;
-    movieList.forEach((movie) => {
-      if (movie.title === movieName) {
-        const characters = movie.characters;
-        characters.forEach((character) => {
-          request(character, (err, res, body) => {
-            if (err) {
-              console.log(err);
-              process.exit();
-            }
-            const character = JSON.parse(body).name;
-            console.log(character);
-          });
-        });
-      }
+  const characters = JSON.parse(body).characters;
+  for (const character of characters) {
+    const actor = await new Promise((resolve, reject) => {
+      request(character, (err, res, body) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(body).name);
+        }
+      });
     });
-  });
+    console.log(actor);
+  }
 }
 
 request(Req, callback);

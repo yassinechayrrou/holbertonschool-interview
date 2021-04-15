f76f8961f9337b5bd1aa220e186f5629c410facb
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-04-16 00:39:58 +0100

    check

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -3,14 +3,15 @@
 
 
 def rain(walls): 
+    """Returns trapped rain water units"""
+    if len(walls) < 3:
+        return 0
     trapped_water = 0
-    left = 0
-    right = len(walls) - 1
-    while left < right:
-        area = min(walls[left], walls[right]) * (right - left)
-        trapped_water = max(trapped_water, area)
-        if walls[left] < walls[right]:
-            left += 1
-        else:
-            right -= 1
+    for i in range(1, len(walls) - 1):
+        left = max(walls[:i])
+        right = max(walls[i + 1:])
+        min_height = min(left, right)
+        if walls[i] < min_height:
+            trapped_water += min_height - walls[i]
     return trapped_water
+

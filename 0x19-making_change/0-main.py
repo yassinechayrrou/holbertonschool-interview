02d787e828d3fd9a254c18811b6b0cda0a09b408
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-06-25 00:51:50 +0100

    fixed using dynamic programming but solution is not optimizedgit add 0-main.py 0-making_change.py !

--- 0x19-making_change/0-main.py
+++ 0x19-making_change/0-main.py
@@ -5,6 +5,11 @@ Main file for testing
 
 makeChange = __import__('0-making_change').makeChange
 
-print(makeChange([1, 2, 25], 37))
+#print(makeChange([1, 2, 25], 37))
 
-print(makeChange([1256, 54, 48, 16, 102], 1453))
+#print(makeChange([1256, 54, 48, 16, 102], 1453))
+print(makeChange([25, 3], 52)) # returns 10
+print(makeChange([1], 2)) # => 2
+print(makeChange([1,2,5], 11)) # => 3
+print(makeChange([25, 3], 52)) # => 10
+print(makeChange([1], 1)) # => 1
--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -2,19 +2,30 @@
 """ Making Change Solution Module """
 
 
+# def makeChange(coins, total):
+#     if total <= 0:
+#         return 0
+#     i = len(coins) - 1
+#     coins.sort()
+#     fewest = []
+#     while i > -1:
+#         fewest.append(coins[i])
+#         if sum(fewest) == total:
+#             return len(fewest)
+#         if sum(fewest) < total:
+#             i += 1
+#         else:
+#             fewest.pop()
+#         i -= 1
+#     return -1
+
 def makeChange(coins, total):
     if total <= 0:
         return 0
-    i = len(coins) - 1
-    coins.sort()
-    fewest = []
-    while i > -1:
-        fewest.append(coins[i])
-        if sum(fewest) == total:
-            return len(fewest)
-        if sum(fewest) < total:
-            i += 1
-        else:
-            fewest.pop()
-        i -= 1
-    return -1
+    dynamic = [total + 1] * (total + 1)
+    dynamic[0] = 0
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                dynamic[i] = min(dynamic[i], 1 + dynamic[i - coin])
+    return dynamic[total] if dynamic[total] != total + 1 else -1

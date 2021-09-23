c066eaf34c4afd2942a3ba82057c1fe5c0b248e7
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-09-23 09:30:18 +0100

    avl

---
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,95 @@
+#include "binary_trees.h"
+
+/**
+ * height - measures the height of a binary tree
+ * @tree: binary tree
+ * Return: height of the @tree
+ */
+size_t height(const binary_tree_t *tree)
+{
+	size_t l_height;
+	size_t r_height;
+
+	if (tree == NULL)
+		return (-1);
+
+	l_height = tree->left ? 1 + height(tree->left) : 0;
+	r_height = tree->right ? 1 + height(tree->right) : 0;
+
+	return (l_height > r_height ? l_height : r_height);
+}
+
+/**
+ * balance - measures the balance factor of binary tree
+ * of a binary tree
+ * @tree: pointer to root node of binary tree
+ * Return: balance factor
+ */
+
+int balance(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (1);
+
+	left = height(tree->left);
+	right = height(tree->right);
+	return (abs(left - right) <= 1
+		&& balance(tree->left)
+		&& balance(tree->right));
+}
+
+
+/**
+ * is_binary_search_tree - checks if is binary search tree helper function
+ * @node: binary tree root node
+ * @min: min in the left subtree
+ * @max: max in the right subtree
+ * Return: 1 or 0
+ */
+
+int is_binary_search_tree(binary_tree_t *node, int min, int max)
+{
+	if (node == NULL)
+		return (1);
+
+	if (node->n < min || node->n > max)
+		return (0);
+
+	return
+		(is_binary_search_tree(node->left, min, node->n - 1) &&
+		 is_binary_search_tree(node->right, node->n + 1, max));
+}
+
+
+/**
+ * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
+ * @tree: binary tree
+ * Return: 1 if @tree is BST, 0 otherwise
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (is_binary_search_tree((binary_tree_t *)tree, INT_MIN, INT_MAX));
+}
+
+
+/**
+ * binary_tree_is_avl - checks if a binary tree is a valid AVL Tree
+ * @tree: binary tree
+ * Return: 1 if tree is a valid AVL Tree, and 0 otherwise
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	int bst, blc;
+
+	if (!tree)
+		return (0);
+
+	bst = binary_tree_is_bst(tree);
+	blc = balance(tree);
+
+	return (bst == 1 && blc == 1);
+}
---
+++ 0x1D-avl_trees/binary_trees.h
@@ -0,0 +1,34 @@
+#ifndef __BINARY_TREES_H__
+#define __BINARY_TREES_H__
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <limits.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
+
+int balance(const binary_tree_t *tree);
+int binary_tree_is_avl(const binary_tree_t *tree);
+int is_binary_search_tree(binary_tree_t *node, int min, int max);
+int binary_tree_is_bst(const binary_tree_t *tree);
+size_t height(const binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *tree);
+
+#endif /* BINARY_TREES_H__ */

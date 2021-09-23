36cfd0cd692742508e0805851278f8b9e2916e5b
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-09-23 11:45:08 +0100

    pascal triangle

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Pascal's triangle problem solution Module """
+
+
+def pascal_triangle(n):
+    """
+    returns pascal triangle of given number n
+    """
+    triangle = []
+    if n <= 0:
+        return triangle 
+    for i in range(n):
+        row = [1]
+        if triangle:
+            last_row = triangle[len(triangle) - 1]
+            computed_row = [sum(pair) for pair in zip(last_row, last_row[1:])]
+            row.extend(computed_row)
+            row.append(1)
+        triangle.append(row)
+    return (triangle)

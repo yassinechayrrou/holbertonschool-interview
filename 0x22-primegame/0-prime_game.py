4b4072b9af60bd27058b72cff470210b6dd3a1b4
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-09-23 16:42:52 +0100

    prime

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,47 +1,25 @@
 #!/usr/bin/python3
-"""primegame Module"""
-
-def is_prime(n):
-    """checks if number is prime"""
-    for i in range(2, (n // 2) + 1):
-        if n % i == 0:
-            return True 
-    return False 
-
-def calc_primes(n, primes):
-    """ extract primes """
-    biggest_prime = primes[-1]
-    if n > biggest_prime:
-        for i in range(biggest_prime + 1, n + 1):
-            if is_prime(i):
-                primes.append(i)
-            else:
-                primes.append(0)
+""" Prime Game Module """
 
 def isWinner(x, nums):
+    """ Returns prime game winner
     """
-    returns the name of player that won
-    """
-    players_wins = {"Maria": 0, "Ben": 0}
+    if not nums or x < 1:
+        return None
 
-    primes = [0, 0, 2]
+    k = 0
+    prime = [0] * 11 ** 5
+    p = [0] * 11 ** 5
 
-    calc_primes(max(nums), primes)
-
-    for round in range(x):
-        sum_options = sum((i != 0 and i <= nums[round])
-                for i in primes[:nums[round] + 1])
-
-        if (sum_options % 2):
-            winner = "Maria"
-        else:
-            winner = "Ben"
-
-        if winner:
-            players_wins[winner] += 1
-
-    if players_wins["Maria"] > players_wins["Ben"]:
-        return "Maria"
-    elif players_wins["Ben"] > players_wins["Maria"]:
-        return "Ben"
+    for i in range(2, 11 ** 5):
+        if p[i] == 0:
+            k += 1
+            for j in range(i, 11 ** 5, i):
+                p[j] = 1
+        prime[i] = k
+    results = True if prime[max(nums)] % 2 == 0 else False
+    #if prime[max(nums)] % 2 == 0:
+    return "Ben" if not results else "Maria"
+    #else:
+    #    return "Maria"
     return None

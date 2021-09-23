76440e5daa8abd4ec4ef142b39db3aa952b6bb80
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-09-23 12:15:07 +0100

    regex working

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,34 @@
+#include "regex.h"
+
+/**
+ * regex_match - checks if pattern matches given string
+ * @str: string to match to
+ * @pattern: regular expression
+ * Return: 1 if pattern do match, else 0
+ */
+
+int regex_match(char const *str, char const *pattern)
+{
+	int dot = 0, star = 0;
+
+	if (!pattern || !str)
+		return (0);
+
+	dot = *str && (*str == *pattern || *pattern == '.');
+	star = *(pattern + 1) == '*';
+
+	if (!*str && !star)
+		if (*pattern)
+			return 0;
+		else
+			return 1;
+
+	if (dot && star)
+		return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
+	if (dot && !star)
+		return (regex_match(str + 1, pattern + 1));
+	if (star)
+		return (regex_match(str, pattern + 2));
+
+	return (0);
+}
---
+++ 0x20-regex/regex.h
@@ -0,0 +1,11 @@
+#ifndef _REGEX_H_
+#define _REGEX_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <regex.h>
+
+int regex_match(char const *str, char const *pattern);
+
+#endif /* _REGEX_H _ */
